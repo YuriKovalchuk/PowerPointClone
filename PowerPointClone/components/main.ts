e861bb22cd86c4af52e2032e92f971c7b6dabd78
Alex Peta <alex@alexpeta.ro>
2015-07-28 14:13:13 +0300

    -- extracted upper page header into a separate react component. -- added "add slide" button to the new upperHeader component

--- PowerPointClone/components/main.ts
+++ PowerPointClone/components/main.ts
@@ -2,7 +2,7 @@
 
 import React = require('react/addons');
 
-
+import UpperMenu = require('../components/upperMenu');
 import SlideBase = require('../components/slideBases');
 import Stage = require('../components/stage');
 import LeftPanel = require('../components/leftSidePanel/LeftPanel');
@@ -29,32 +29,7 @@ class Main extends React.Component<any, any, any>
     render() {
         return React.jsx(`
 <div>	
-	<nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div className="container">
-            <div className="navbar-header">
-                <button type="button" className="navbar-toggle">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="#">Powerpoint Clone</a>
-            </div>
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav">
-                    <li>
-                        <a href="#">Menu Item 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Menu Item 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Menu Item 3</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <UpperMenu />
     <div className="container">
         <div className="row">
             <div className="col-md-2">
---
+++ PowerPointClone/components/upperMenu.ts
@@ -0,0 +1,37 @@
+﻿///<reference path="../lib/_references.d.ts"/>
+
+import React = require('react/addons');
+
+class UpperMenu extends React.Component<any, any, any>
+{
+    addNewSlideHandler() {
+        console.log('add new slide');
+    }
+    
+    render() {
+        return React.jsx(`
+	        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div className="container">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">PowerPoint Clone</a>
+                    </div>
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                            <li>
+                                <a onClick={this.addNewSlideHandler.bind(this)}>Add New Slide</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+         `);
+    }
+}
+
+export = UpperMenu;
