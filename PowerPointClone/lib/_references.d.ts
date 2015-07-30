0952d0a5e057cf58ec6f0604fa1d4e4d0ba858a6
Stefanita-Alexandru Mitran <stefanita.mitran@hotmail.com>
2015-07-30 11:11:50 +0300

    --added backbone

--- PowerPointClone/lib/_references.d.ts
+++ PowerPointClone/lib/_references.d.ts
@@ -1,3 +1,6 @@
-///<reference path="node.webpack.ts" />
-///<reference path="react-0.13.0.d.ts"/>
-///<reference path="react-jsx.d.ts"/>
+/// <reference path="react-0.13.0.d.ts"/>
+/// <reference path="react-jsx.d.ts"/>
+/// <reference path="jquery.d.ts" />
+/// <reference path="underscore.d.ts" />
+/// <reference path="backbone.d.ts" />
+/// <reference path="../dal/repositorymanager.ts" />
